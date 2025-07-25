789c290a7a8e8180e6b39abbc8d08331c24a610a
imalishehara <146943438+imalishehara@users.noreply.github.com>
2025-07-25 20:38:49 +0530

    Update VolunteerContributionDTO.cs

--- Disaster_demo/Models/Entities/VolunteerContributionDTO.cs
+++ Disaster_demo/Models/Entities/VolunteerContributionDTO.cs
@@ -1,4 +1,4 @@
-﻿namespace Disaster_demo.Models.Entities
+namespace Disaster_demo.Models.Entities
 {
     public class VolunteerContributionDTO
     {
@@ -7,9 +7,11 @@
         public string volunteer_name { get; set; }
         public string volunteer_contact { get; set; }
         public string district { get; set; }
-        public string type_support { get; set; }
+        public string VolunteerTypeSupport { get; set; }
+        public string AidRequestTypeSupport { get; set; }
         public string description { get; set; }
         public string? image { get; set; }
         public string status { get; set; }
+        public string requester_nic { get; set; }
     }
 }
