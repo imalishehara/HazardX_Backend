ea90d7a594c9af472fa2fda53dca5a15d31fb6f3
imalishehara <146943438+imalishehara@users.noreply.github.com>
2025-07-25 18:52:40 +0530

    Update IAidRequestServices.cs

--- Disaster_demo/Services/IAidRequestServices.cs
+++ Disaster_demo/Services/IAidRequestServices.cs
@@ -1,19 +1,1 @@
-using Disaster_demo.Models.Entities;
 
-namespace Disaster_demo.Services
-{
-    public interface IAidRequestServices
-    {
-        Task<bool> CreateAidRequestAsync(AidRequests request);
-        Task<int> GetContributionCountAsync(int aidId);
-        Task<List<AidRequests>> GetDeliveredAidRequestsAsync();
-        Task<List<AidRequests>> GetDsApprovedAidRequests();
-        Task<(List<AidRequests> dsApprovedPostDisaster, List<AidRequests> emergency)> GetDsApprovedPostDisasterAndEmergencyAidRequestsByDistrictAsync(string district, bool? isFulfilled = null);
-        Task<List<AidRequests>> GetOngoingAidRequestsAsync(string? divisionalSecretariat = null);
-        Task<List<AidRequests>> GetPendingEmergencyAidRequestsAsync();
-        Task<List<AidRequests>> GetPendingPostDisasterAidRequestsAsync(string divisionalSecretariat);
-        Task<int> GetPendingPostDisasterAidRequestsCountAsync(string divisionalSecretariat);
-        Task<bool> MarkAidRequestAsResolvedAsync(int aidId);
-        bool UpdateStatus(StatusUpdateModel model);
-    }
-}
