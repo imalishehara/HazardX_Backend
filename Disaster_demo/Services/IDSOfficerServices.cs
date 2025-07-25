779ca0a7808c54a50ddd34e5b492f1dc7a23e7b5
imalishehara <146943438+imalishehara@users.noreply.github.com>
2025-07-25 18:53:28 +0530

    Update IDSOfficerServices.cs

--- Disaster_demo/Services/IDSOfficerServices.cs
+++ Disaster_demo/Services/IDSOfficerServices.cs
@@ -1,10 +1,1 @@
-using Disaster_demo.Models.Entities;
 
-namespace Disaster_demo.Services
-{
-    public interface IDSOfficerServices
-    {
-        Task<LoginResponseDTO?> GetDsOfficerDetailsAsync(string userId);
-        Task<List<DS_Officer>> GetAllDsOfficersAsync();
-    }
-}
